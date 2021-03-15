ff1685b8650493d62c002561d387dfed43c70d44
Kevin Tian <ktian0257@gmail.com>
2021-03-15 01:36:11 -0700

    resume added

--- components/content-components/hero.vue
+++ components/content-components/hero.vue
@@ -4,7 +4,14 @@
       <div class="container">
         <h1 class="title is-1">Kevin Tian</h1>
         <h2 class="subtitle is-3">Front End Software Engineer</h2>
-        <a class="button is-light is-primary">Resume</a>
+        <a
+          href="~assets/Resume.pdf"
+          target="_blank"
+          rel="noopener noreferrer"
+          class="button is-light is-primary"
+        >
+          Resume
+        </a>
       </div>
     </div>
   </section>
